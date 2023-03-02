b67151ffb8e9330a52eed8d7ec6822b39452e1a1
Rajiya Shaik <124450567+Rajiya243@users.noreply.github.com>
2023-03-02 23:41:42 +0530

    Add files via upload

---
+++ src/main/java/com/driver/encapsulation-Rajiya243/src/main/java/com/driver/Main.java
@@ -0,0 +1,12 @@
+package com.driver;
+
+public class Main {
+    public static void main(String [] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("Rajiya");
+        System.out.println(obj.getName());
+
+    }
+
+  
+}
